ffe21f16a36e219ad8d48e99ecc9549b99655c98
gabrielemartire <gabriele.martire@gmail.com>
2025-08-28 07:10:34 +0200

    Add POST endpoint tests for task creation

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Tasks", type: :request do
-    # index test
+    # GET
     describe "GET /tasks" do
         it "returns http success" do
         get "/tasks"
@@ -14,5 +14,56 @@ RSpec.describe "Tasks", type: :request do
             get "/tasks.json"
             expect(response.content_type).to eq("application/json; charset=utf-8")
         end
+    end
+
+    # POST
+    describe "POST /tasks" do
+        context "with valid parameters" do
+            it "creates a new task" do
+                project = create(:project)
+                
+                expect {
+                    post project_tasks_path(project), params: { 
+                        task: { 
+                            title: "Test Task",
+                            due_date: Date.tomorrow,
+                            priority: 3
+                        } 
+                    }
+                }.to change(Task, :count).by(1)
+            end
+        end
+
+        context "with invalid parameters" do
+            it "does not create a task" do
+                project = create(:project)
+                
+                expect {
+                post project_tasks_path(project), params: { 
+                    task: { 
+                        title: "", # invalid
+                        priority: 3
+                    } 
+                }
+                }.not_to change(Task, :count)
+            end
+
+            it "returns unprocessable_entity for JSON format" do
+                project = create(:project)
+                
+                post project_tasks_path(project, format: :json), params: { 
+                    task: { 
+                        title: "",
+                        priority: 3
+                    } 
+                }
+                
+                expect(response).to have_http_status(:unprocessable_entity)
+                expect(response.content_type).to eq("application/json; charset=utf-8")
+            end
+        end
+
+
+
     end
 end
